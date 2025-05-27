209f2c7d396ec9f0c3fe91546c4ad908833853c2
Samsonov Dmitrii <samdim2011@mail.ru>
2025-05-27 12:46:26 +0700

    third

--- contactForm/src/main/resources/db/migration/V2__create_publication.sql
+++ contactForm/src/main/resources/db/migration/V2__create_publication.sql
@@ -1,1 +1,6 @@
-CREATE PUBLICATION contacts_outbox FOR TABLE contacts;
+CREATE TABLE IF NOT EXISTS debezium_heartbeat(
+                                              id INTEGER PRIMARY KEY,
+                                              updated_at TIMESTAMP NOT NULL
+);
+
+CREATE PUBLICATION contacts_outbox FOR TABLE contacts,debezium_heartbeat;
